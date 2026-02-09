e8af439994ae7b8cabb48a5a53a1cc778ca57fff
Henry <ewwhenry@proton.me>
2026-02-09 15:42:17 -0700

    fix(deferReply): Now the bot will be waiting for a reply in order to evade the timeout by Discord.

--- src/events/buttons.interactionCreate.ts
+++ src/events/buttons.interactionCreate.ts
@@ -17,6 +17,7 @@ export default defineEvent({
     if (!guildConfig) return;
 
     if (buttonId.startsWith("tickets:")) {
+      await interaction.deferReply();
       if (!guildConfig.ticketsParentChannelId) return;
       const ticketSystem = new Tickets(
         interaction.guildId,
